c8e8e9b1a925911fac268ba307fab75019bf766f
santiocampo1 <sanntiocampo@gmail.com>
2024-07-31 07:53:16 -0300

    Get by Id, Post and Put controllers

--- src/main/java/com/apirest/apirest/Controllers/ProductController.java
+++ src/main/java/com/apirest/apirest/Controllers/ProductController.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,25 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Product not found for id :: " + id));
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        Product product = productRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Product not found for id :: " + id));
+
+        product.setName(productDetails.getName());
+        product.setPrice(productDetails.getPrice());
+
+        return productRepository.save(product);
+    }
 }
